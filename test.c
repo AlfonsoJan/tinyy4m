47758111f34695e4307884fe374fc494bf83cfaf
alfonsojan <alfonsobusker@gmail.com>
2025-09-04 20:20:21 +0200

    Initial Commit

---
+++ test.c
@@ -0,0 +1,37 @@
+#define TINYY4MDEF static inline
+#define TINYY4M_IMPLEMENTATION
+#include "tinyy4m.h"
+
+#define WIDTH 1600
+#define HEIGHT 900
+#define N WIDTH*HEIGHT
+#define FPS 60
+#define DURATION_SECONDS 5
+#define TOTAL_FRAMES FPS * DURATION_SECONDS
+
+static uint32_t pixels[WIDTH * HEIGHT];
+static uint8_t Y[N], U[N], V[N];
+
+void fill_pixels(uint32_t color) {
+    for (size_t y = 0; y < HEIGHT; ++y) {
+        for (size_t x = 0; x < WIDTH; ++x) {
+            pixels[y * WIDTH + x] = color;
+        }
+    }
+}
+
+int main() {
+    // FORMAT RRGGBB
+    fill_pixels(0xFF0000);
+    Y4MWriter writer = {0};
+    if(y4m_start("out.y4m", &writer, WIDTH, HEIGHT, FPS, Y, U, V) != 0) {
+        // LOG ERROR
+        return -1;
+    }
+
+    for (int frame = 0; frame < TOTAL_FRAMES; frame++) {
+        y4m_write_frame(&writer, pixels);
+    }
+
+    y4m_end(&writer);
+}
---
+++ tinyy4m.h
@@ -0,0 +1,85 @@
+#ifndef TINYY4M_H_
+#define TINYY4M_H_
+
+#include <stdio.h>
+#include <stdint.h>
+
+#ifndef TINYY4MDEF
+/*
+   Define TINYY4MDEF before including this file to control function linkage.
+   Example: #define TINYY4MDEF static inline
+   This is useful for single-file projects to let the compiler inline functions
+   and remove unused ones.
+*/
+#define TINYY4MDEF
+#endif /* TINYY4MDEF */
+
+typedef struct {
+    FILE *f;
+    int width, height;
+    uint8_t *y_plane;
+    uint8_t *u_plane;
+    uint8_t *v_plane;
+} Y4MWriter;
+
+TINYY4MDEF int y4m_start(const char *filename, Y4MWriter *w, const int width, const int height, const int fps, uint8_t *y, uint8_t *u, uint8_t *v);
+TINYY4MDEF void y4m_write_frame(Y4MWriter *w, uint32_t *pixels);
+TINYY4MDEF void y4m_end(Y4MWriter *w);
+
+#ifdef TINYY4M_IMPLEMENTATION
+
+TINYY4MDEF int y4m_start(const char *filename, Y4MWriter *w, const int width, const int height, const int fps, uint8_t *y, uint8_t *u, uint8_t *v) {
+
+    w->f = fopen(filename, "wb");
+    if (!w->f) {
+        return 1;
+    }
+
+    w->width = width;
+    w->height = height;
+
+    w->y_plane = y;
+    w->u_plane = u;
+    w->v_plane = v;
+
+    fprintf(w->f, "YUV4MPEG2 W%d H%d F%d:1 Ip A1:1 C444\n", width, height, fps);
+    return 0;
+}
+
+TINYY4MDEF void y4m_write_frame(Y4MWriter *w, uint32_t *pixels) {
+    fprintf(w->f, "FRAME\n");
+
+    const size_t N = (size_t)w->width * (size_t)w->height;
+    for (size_t i = 0; i < N; ++i) {
+        uint8_t r = (pixels[i] >> 16) & 0xFF;
+        uint8_t g = (pixels[i] >> 8) & 0xFF;
+        uint8_t b = (pixels[i] >> 0) & 0xFF;
+
+        float Yf =  0.299f * r + 0.587f * g + 0.114f * b;
+        float Uf = -0.169f * r - 0.331f * g + 0.500f * b + 128.0f;
+        float Vf =  0.500f * r - 0.419f * g - 0.081f * b + 128.0f;
+
+        /* Clamp to [0,255] */
+        uint8_t Y = (uint8_t)(Yf < 0 ? 0 : (Yf > 255 ? 255 : Yf));
+        uint8_t U = (uint8_t)(Uf < 0 ? 0 : (Uf > 255 ? 255 : Uf));
+        uint8_t V = (uint8_t)(Vf < 0 ? 0 : (Vf > 255 ? 255 : Vf));
+
+        w->y_plane[i] = Y;
+        w->u_plane[i] = U;
+        w->v_plane[i] = V;
+    }
+
+    fwrite(w->y_plane, 1, N, w->f);
+    fwrite(w->u_plane, 1, N, w->f);
+    fwrite(w->v_plane, 1, N, w->f);
+}
+
+TINYY4MDEF void y4m_end(Y4MWriter *w) {
+    if (!w) return;
+    fclose(w->f);
+    w->f = NULL;
+}
+
+#endif // TINYY4M_IMPLEMENTATION
+
+#endif // TINYY4M_H_
